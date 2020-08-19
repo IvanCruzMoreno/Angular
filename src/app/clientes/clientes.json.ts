531a837cece482106707d4aa4b19b1572ae2db2b
Ivan <ivanmoreno405@gmail.com>
2020-08-19 12:48:17 -0500

    i separated the service from the component. also, i added a json file contain' customers list

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import {Cliente} from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id:1, nombre:'Ivan',apellido:'Moreno',email:'ivanmoreno@bad.com',fecha:'2020-08-08'},
+  {id:2, nombre:'carlos',apellido:'enomo',email:'aaao@bad.com',fecha:'2020-02-02'},
+  {id:3, nombre:'narvi',apellido:'morono',email:'ino@bad.com',fecha:'2020-11-17'},
+  {id:4, nombre:'locars',apellido:'oiui',email:'chi@bad.com',fecha:'2020-06-06'},
+  {id:5, nombre:'vani',apellido:'erds',email:'ca@bad.com',fecha:'2020-03-03'},
+  {id:6, nombre:'navi',apellido:'redsno',email:'cho@bad.com',fecha:'2020-09-09'},
+];
