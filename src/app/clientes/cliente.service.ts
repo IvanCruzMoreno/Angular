f298e1e3966281fc392210ca066642b2def84fbe
Ivan <ivanmoreno405@gmail.com>
2020-08-20 16:25:39 -0500

    i added what is needed to access the back-end (HtttpClient, urlEndPoint and HttpClientModule)

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,13 +2,19 @@ import { Injectable } from '@angular/core';
 import {CLIENTES} from './clientes.json';
 import {Cliente} from './cliente';
 import {Observable,of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class ClienteService{
 
-  constructor() {
+  private http: HttpClient;
+  private urlEndPoint: string = 'http://localhost:8090/api/clientes';
+
+  constructor(http: HttpClient) {
+    this.http = http;
   }
   getClientes(): Observable<Cliente[]>{
-    return of(CLIENTES);
+    //return of(CLIENTES);
+    return this.http.get<Cliente[]>(this.urlEndPoint);
   }
 }
