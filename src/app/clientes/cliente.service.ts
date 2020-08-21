8eb9c60f16a0c561aece97cbe597f9c9851ab2d4
Ivan <ivanmoreno405@gmail.com>
2020-08-21 12:00:01 -0500

    i created the views to create a new "Cliente" also i implemented the get and post methods to access the back-end

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import {CLIENTES} from './clientes.json';
 import {Cliente} from './cliente';
 import {Observable,of} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable()
 export class ClienteService{
 
   private http: HttpClient;
   private urlEndPoint: string = 'http://localhost:8090/api/clientes';
+  private httpHeader = new HttpHeaders({'Content-Type': 'application/json'});
 
   constructor(http: HttpClient) {
     this.http = http;
@@ -16,5 +17,8 @@ export class ClienteService{
   getClientes(): Observable<Cliente[]>{
     //return of(CLIENTES);
     return this.http.get<Cliente[]>(this.urlEndPoint);
+  }
+  createCliente(cliente: Cliente): Observable<Cliente>{
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers:this.httpHeader});
   }
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -9,13 +11,19 @@ export class FormComponent implements OnInit {
 
   public cliente: Cliente = new Cliente();
   public titulo: string = "Crear Cliente";
+  private clienteService: ClienteService;
+  private router: Router;
 
-  constructor() { }
-
+  constructor(clienteService: ClienteService, router: Router) {
+    this.clienteService = clienteService;
+    this.router = router;
+  }
   ngOnInit(): void {
   }
   public create():void{
-    console.log("Cliked!");
-    console.log(this.cliente);
+    //console.log("Cliked!");
+    //console.log(this.cliente);
+    this.clienteService.createCliente(this.cliente).subscribe( _response => this.router.navigate(['/clientes']));
+
   }
 }
